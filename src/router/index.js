1a2701a3dd7176055ae79caa8d0c0e8c44c3e781
Deepti K <deeptik.itpath@gmail.com>
2023-03-24 12:10:11 +0530

    did changes due to server error

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import { Router } from "express";
+import Router from "express";
 import { responseData, statusCode } from "../helper/helper.js";
-import {createContactForm} from "../controller/users.js";
+import { createContactForm } from "../controller/users.js";
 
 
 const router = Router();
